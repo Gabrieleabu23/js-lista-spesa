3fd51d19cc3718abb6f12bc83cbabbc773ac9f42
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-11-24 18:54:40 +0100

    add: chance to input array list length

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,8 +1,13 @@
-const listaSpesa=["Latte","Formaggio","Burro","Farina"];
+const listaSpesa=[];
+
+listaSpesa.length= prompt("Quanti prodotti vuoi mettere all'interno della lista?");
+console.log(listaSpesa.length)
 const stampaLista= document.getElementById("lista_spesa");
 // stampaLista.append(elementoSpesa);
 let i=0;
 while(i< listaSpesa.length){
+    listaSpesa[i]=prompt("Inserisci prodotti da mettere nella lista, max 4");
+    console.log(listaSpesa[i])
     const elementoSpesa= document.createElement("li");
     elementoSpesa.append(listaSpesa[i]);
     stampaLista.append(elementoSpesa);
